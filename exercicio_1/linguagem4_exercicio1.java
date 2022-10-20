468f32668232e385100eca3abc42b295874f1abb
Juan Gomes <67767706+JuanGomes404@users.noreply.github.com>
2022-10-20 14:56:48 -0300

    Update linguagem4_exercicio1.java

--- exercicio_1/linguagem4_exercicio1.java
+++ exercicio_1/linguagem4_exercicio1.java
@@ -1,25 +1,33 @@
-public static void main(String args[]){
-int[] vet = {8, 9, 3, 5, 1};
-int aux = 0;
-int i = 0;
+public static void main(String args[]) {
+  
+  //BUBBLE SORT -> COMPLEXIDADE: O(N^2)
+   int[] vet = {
+      8,
+      9,
+      3,
+      5,
+      1
+   };
+   int aux = 0;
+   int i = 0;
 
-System.out.println("Vetor desordenado: ");
-for(i = 0; i<5; i++){
-System.out.println(" "+vet[i]);
-}
-System.out.println(" ");
+   System.out.println("Vetor desordenado: ");
+   for (i = 0; i < 5; i++) {
+      System.out.println(" " + vet[i]);
+   }
+   System.out.println(" ");
 
-for(i = 0; i<5; i++){
-for(int j = 0; j<4; j++){
-if(vet[j] > vet[j + 1]){
-aux = vet[j];
-vet[j] = vet[j+1];
-vet[j+1] = aux;
-}
-}
-}
-System.out.println("Vetor organizado:");
-for(i = 0; i<5; i++){
-System.out.println(" "+vet[i]);
-}
+   for (i = 0; i < 5; i++) {
+      for (int j = 0; j < 4; j++) {
+         if (vet[j] > vet[j + 1]) {
+            aux = vet[j];
+            vet[j] = vet[j + 1];
+            vet[j + 1] = aux;
+         }
+      }
+   }
+   System.out.println("Vetor organizado:");
+   for (i = 0; i < 5; i++) {
+      System.out.println(" " + vet[i]);
+   }
 }
